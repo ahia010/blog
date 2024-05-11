b1085ff123eec3ce63b49fafd418ffbcc070f9be
ahia010 <92839414+ahia010@users.noreply.github.com>
2024-05-11 23:36:09 +0800

    完善vue端

--- vue-blog/src/main.js
+++ vue-blog/src/main.js
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import NConfigProvider from 'naive-ui'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +12,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(NConfigProvider)
 app.mount('#app')
