3a0ba45504b7803cebf99f94bda1a30d807fc220
ahia010 <92839414+ahia010@users.noreply.github.com>
2024-05-19 00:17:43 +0800

    初始化首页

--- vue-blog/src/App.vue
+++ vue-blog/src/App.vue
@@ -1,71 +1,119 @@
+<template>
+  <n-layout>
+    <n-layout-header bordered class="nav">
+      <div class="nav-left">
+        <div class="logo">
+          
+        </div>
+        <n-menu class="home-menu" v-model:value="activeKey" mode="horizontal" :options="menuOptions" responsive/>
+
+      </div>
+      <div class="header-center">
+        <n-input size="medium" round placeholder="搜索" class="header-input">
+          <template #suffix>
+            <!--          <n-icon :component="FlashOutline" />-->
+          </template>
+        </n-input>
+      </div>
+      <div class="nav-right">
+        <n-button type="text">登录</n-button>
+        <n-button type="text">注册</n-button>
+      </div>
+    </n-layout-header>
+    <n-layout-content>
+      <router-view/>
+    </n-layout-content>
+    <n-layout-footer>底部</n-layout-footer>
+  </n-layout>
+</template>
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {h, ref} from "vue";
+import {useRouter, useRoute} from "vue-router";
+
+const router = useRouter()
+
+const route = useRoute()
+
+const activeKey = ref(route.path)
+
+const menuOptions = [
+  {
+    label: () => h(
+        "router-link",
+        {
+          href: "/",
+        },
+        "首页"
+    ),
+    key: "/",
+  },
+  {
+    label: "寻羊冒险记",
+    key: "/kind",
+  },
+];
 </script>
 
-<template>
-  <RouterView />
-</template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.header-center{
+  flex: 1;
+}
+.n-layout-header {
+  padding: 0 32px;
+  height: 64px;
+  display: flex;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.nav-left {
+  margin-left: 50px;
+  display: flex;
+  align-items: center;
+
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.header-input {
+  margin: 0 20px;
+  width: 500px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.header-input {
+
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.nav-right {
+  white-space: pre;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+/**
+:deep(.n-menu) {
+
 }
 
-nav a:first-of-type {
+
+
+:deep(.v-overflow) {
+  height: 100%;
+  align-items: center;
+}
+
+:deep(.n-menu-item) {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+
+:deep(.n-menu.n-menu--horizontal .n-menu-item-content::before){
+  display: flex;
+}
+:deep(.n-menu.n-menu--horizontal .n-menu-item-content){
   border: 0;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+:deep(.n-menu.n-menu--horizontal .n-menu-item-content:not(.n-menu-item-content--disabled):hover){
+  border: none;
 }
+
+ */
 </style>
